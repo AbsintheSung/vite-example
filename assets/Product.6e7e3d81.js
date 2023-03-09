50037a866ab1f5232b01f2ae3013b86e836d275d
AbsintheSung <h90243768@gmail.com>
2023-03-09 21:52:12 +0800

    adding dist

---
+++ assets/Product.6e7e3d81.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,b as o,d as a,R as c}from"./index.bdc25c8b.js";const r={class:"container"},d={__name:"Product",setup(s){return(n,_)=>(e(),t("div",r,[o(a(c))]))}};export{d as default};
